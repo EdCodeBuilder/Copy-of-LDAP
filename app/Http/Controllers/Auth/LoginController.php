b417632a54b39a07d12da24e0a5fc7223d0ba8d2
Daniel Prado <danii.prado@outlook.com.ar>
2020-09-25 15:43:45 -0500

    Middleware para las rutas de Api

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -196,8 +196,7 @@ class LoginController extends Controller
             'client_secret' =>  env('PASSPORT_CLIENT_SECRET'),
             'grant_type'    =>  env('PASSPORT_GRANT_TYPE'),
         ]);
-        $data = (new DiactorosFactory)->createRequest( $request );
-        return app( AccessTokenController::class )->issueToken($data);
+        return app( AccessTokenController::class )->issueToken($request->server());
     }
 
     /**
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,11 +37,17 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'signature',
+            'localization',
+            'json.response'
         ],
 
         'api' => [
-            'throttle:60,1',
+            'api.throttle:60,1',
             'bindings',
+            'signature',
+            'localization',
+            'json.response',
         ],
     ];
 
