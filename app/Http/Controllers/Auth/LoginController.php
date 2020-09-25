9897c89635865cd32e69355a82d8a49deb9abfa5
Daniel Prado <danii.prado@outlook.com.ar>
2020-09-25 11:40:42 -0500

    Control de errores de validación del LDAP

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -91,4 +91,17 @@ class LoginController extends Controller
     {
         return 'username';
     }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        return $this->guard()->attempt(
+            $this->credentials($request), $request->filled('remember')
+        );
+    }
 }
