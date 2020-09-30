e642ad7a69a8932b75b339c9d2c4d5556534b4e4
Daniel Prado <danii.prado@outlook.com.ar>
2020-09-30 13:36:10 -0500

    Corrección de ruta invokable

--- routes/sub_routes/global/global.php
+++ routes/sub_routes/global/global.php
@@ -28,6 +28,6 @@ Route::middleware('auth:api')->prefix('api')->group( function () {
     Route::post('logout', [LoginController::class, 'logout'])->name('passport.logout');
     Route::post('logout-all-devices', [LoginController::class, 'logoutAllDevices'])->name('passport.logout.all');
     Route::prefix('admin')->group( function () {
-        Route::middleware('can:sync-users')->get('sync-users', ActiveDirectoryController::class)->name('admin.sync.users');
+        Route::middleware('can:sync-users')->get('sync-users', [ActiveDirectoryController::class])->name('admin.sync.users');
     });
 });
