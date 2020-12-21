bb574d3bd56401de37c2ceb2c8a486e946ed523b
Daniel Prado <danii.prado@outlook.com.ar>
2020-12-21 11:39:55 -0500

    Creación del vector para iniciar sesión

--- app/Http/Resources/Auth/ActivityPermissionResource.php
+++ app/Http/Resources/Auth/ActivityPermissionResource.php
@@ -18,6 +18,7 @@ class ActivityPermissionResource extends JsonResource
             'user_id'       =>  isset( $this->Id_Persona ) ? (int) $this->Id_Persona : null,
             'activity_id'   =>  isset( $this->Id_Actividad ) ? (int) $this->Id_Actividad : null,
             'status'        =>  isset( $this->Estado ) ? (bool) $this->Estado : null,
+            'status_int'    =>  isset( $this->Estado ) ? (int) $this->Estado : null,
             'created_at'    =>  isset( $this->TimeStamp ) ? $this->TimeStamp->format('Y-m-d H:i:s') : null,
         ];
     }
--- app/Http/Resources/Auth/ActivityResource.php
+++ app/Http/Resources/Auth/ActivityResource.php
@@ -14,13 +14,19 @@ class ActivityResource extends JsonResource
      */
     public function toArray($request)
     {
+        $permissions = ActivityPermissionResource::collection( $this->whenLoaded('permission') );
+        $vector = [];
+        $vector[0] = auth()->check() && auth()->user()->sim_id ? auth()->user()->sim_id : null;
         return [
             'id'        =>  isset( $this->Id_Actividad ) ? (int) $this->Id_Actividad : null,
             'name'      =>  isset( $this->Nombre_Actividad ) ? $this->Nombre_Actividad : null,
             'description'      =>  isset( $this->Descripcion ) ? $this->Descripcion : null,
             'module_id' =>  isset( $this->Id_Modulo ) ? (int) $this->Id_Modulo : null,
             'module'    => new ModuleResource( $this->whenLoaded('module') ),
-            'permission'    => ActivityPermissionResource::collection( $this->whenLoaded('permission') ),
+            'permission'   => $permissions,
+            'encoded'      => auth()->check()
+                        ? urlencode( serialize( array_merge( $vector, $permissions->collection->pluck('status_int')->toArray() ) ) )
+                        : []
         ];
     }
 }
