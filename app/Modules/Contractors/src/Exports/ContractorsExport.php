7a7c8392cac000f95e4b25ddb7ab062eea1a5330
Daniel Prado <danii.prado@outlook.com.ar>
2021-03-18 15:56:09 -0500

    Filtro archivo excel

--- app/Modules/Contractors/src/Exports/ContractorsExport.php
+++ app/Modules/Contractors/src/Exports/ContractorsExport.php
@@ -147,25 +147,42 @@ class ContractorsExport implements FromQuery, WithMapping, WithHeadings
 
     public function queryBuilder(Request $request, Builder $builder)
     {
-        return $builder->when($request->has('has_arl'), function ($q) {
-            return $q->whereHas('contracts', function ($query) {
-                return $query->whereHas('files', function ($query) {
-                    return $query->where('file_type_id', 1);
-                });
+        return $builder->when($request->has('doesnt_have_arl'), function ($q) {
+            return $q->whereNull('modifiable')->whereHas('contracts', function ($query) {
+                return $query->where('contract_type_id', '!=', 3)
+                    ->whereDate('final_date', '>=', now()->format('Y-m-d'))
+                    ->withCount([
+                        'files as arl_files_count' => function ($q) {
+                            return $q->where('file_type_id', 1);
+                        },
+                        'files as other_files_count' => function ($q) {
+                            return $q->where('file_type_id', '!=', 1);
+                        },
+                    ])->having('arl_files_count', 0);
             });
-        })->when($request->has('has_secop'), function ($q) {
+        })->when($request->has('doesnt_have_secop'), function ($q) {
             return $q->whereHas('contracts', function ($query) {
-                return $query->whereHas('files', function ($query) {
-                    return $query->where('file_type_id', 2);
-                });
+                return $query->where('contract_type_id', '!=', 3)
+                    ->whereDate('final_date', '>=', now()->format('Y-m-d'))
+                    ->withCount([
+                        'files as arl_files_count' => function ($q) {
+                            return $q->where('file_type_id', 1);
+                        },
+                        'files as other_files_count' => function ($q) {
+                            return $q->where('file_type_id', '!=', 1);
+                        },
+                    ])->having('other_files_count', 0);
             });
         })->when($request->has('query'), function ($q) use ($request) {
             $data = toLower($request->get('query'));
             return $q->whereHas('contracts', function ($query) use ($data) {
                 return $query->where('contract', 'like', "%{$data}%");
             })->orWhere('name', 'like', "%{$data}%")
+                ->orWhere('id', 'like', "%{$data}%")
                 ->orWhere('surname', 'like', "%{$data}%")
                 ->orWhere('document', 'like', "%{$data}%");
+        })->when($request->has('doesnt_have_data'), function ($q) use ($request) {
+            return $q->whereNotNull('modifiable');
         });
     }
 
