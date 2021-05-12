b072daedb4a18ae8970f210ece4584f0a55cca5b
Daniel Prado <danii.prado@outlook.com.ar>
2021-05-12 15:47:32 -0500

    Validación Fechas Test

--- app/Modules/Contractors/src/Controllers/PeaceAndSafeController.php
+++ app/Modules/Contractors/src/Controllers/PeaceAndSafeController.php
@@ -277,10 +277,9 @@ class PeaceAndSafeController extends Controller
             $username = isset($user->usua_login) ? $user->usua_login : 0;
             if ($this->hasLDAP($username) ) {
                 $new_expire_date = ldapDateToCarbon( $this->user->getFirstAttribute('accountexpires') );
-                $date = isset($expires_at) ? $expires_at : $new_expire_date;
                 if (
                     $this->accountIsActive() &&
-                    $this->cantCreateDocument($date) &&
+                    $this->cantCreateDocument($expires_at) &&
                     !(isset($expires_at) && Carbon::parse($new_expire_date)->diffInDays($expires_at, false) <= 3)
                 ) {
                     return $this->error_response(
@@ -297,7 +296,7 @@ class PeaceAndSafeController extends Controller
             if ( $total['total'] > 0 ) {
                 $certification->expires_at = ldapDateToCarbon( $this->user->getFirstAttribute('accountexpires') );
                 $certification->save();
-                // array_push($total, ['result' => $this->cantCreateDocument($expires_at)]);
+                array_push($total, ['result' => $this->cantCreateDocument($expires_at)]);
                 return $this->error_response(
                     "Para generar el paz y salvo de sistemas debe tener sus bandejas de Orfeo en cero, actualmente cuenta con {$total['total']} radicado(s) sin procesar.",
                     Response::HTTP_UNPROCESSABLE_ENTITY,
@@ -424,7 +423,7 @@ class PeaceAndSafeController extends Controller
 
     public function cantCreateDocument($expires_at = null)
     {
-        return $this->canCreateDocument($expires_at);
+        return ! $this->canCreateDocument($expires_at);
     }
 
     /**
