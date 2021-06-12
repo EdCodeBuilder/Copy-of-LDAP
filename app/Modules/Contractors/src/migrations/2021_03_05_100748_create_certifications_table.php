9096d00ae4b6ba98621efd75938d56aa0bbe8785
Daniel Prado <danii.prado@outlook.com.ar>
2021-06-11 20:44:01 -0500

    Corrección Migración

--- app/Modules/Contractors/src/migrations/2021_03_05_100748_create_certifications_table.php
+++ app/Modules/Contractors/src/migrations/2021_03_05_100748_create_certifications_table.php
@@ -15,13 +15,15 @@ class CreateCertificationsTable extends Migration
     {
         Schema::create('certifications', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name', 191)->comment('Nombre del solicitante');
-            $table->string('document', 20)->comment('Número de documento del solicitante');
-            $table->string('contract', 30)->comment('Número de Contrato para el certificado');
-            $table->string('virtual_file', 50)->comment('Expediente virtual del contrato');
-            $table->string('token', 20)->comment('Identificador random del documento');
-            $table->date('expires_at')->comment('Fecha de finalización del contrato');
-            $table->string('type', 3)->comment('Tipo de certificado');
+            $table->string('name', 191)->nullable()->comment('Nombre del solicitante');
+            $table->string('document', 20)->nullable()->comment('Número de documento del solicitante');
+            $table->string('contract', 30)->nullable()->comment('Número de Contrato para el certificado');
+            $table->string('virtual_file', 50)->nullable()->comment('Expediente virtual del contrato');
+            $table->string('token', 20)->nullable()->comment('Identificador random del documento');
+            $table->date('expires_at')->nullable()->comment('Fecha de finalización del contrato');
+            $table->string('type', 3)->nullable()->comment('Tipo de certificado');
+            $table->string('code', 9)->nullable()->comment('Código de verficación Alamacén');
+            $table->unsignedInteger('downloads')->default(0)->nullable()->comment('Cantidad de Descargas');
             $table->timestamps();
         });
     }
