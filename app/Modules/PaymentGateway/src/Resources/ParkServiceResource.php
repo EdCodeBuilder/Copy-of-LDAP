5b8839721e1a34b2fc30761a7960871667187047
Jhon Jairo Zabala <jhonnyzb1983@gmail.com>
2022-02-11 18:22:53 -0500

    ajuste controller paymentz

--- app/Modules/PaymentGateway/src/Resources/ParkServiceResource.php
+++ app/Modules/PaymentGateway/src/Resources/ParkServiceResource.php
@@ -15,9 +15,11 @@ class ParkServiceResource extends JsonResource
       public function toArray($request)
       {
             return [
-                  'id' => $this->id_parque_servicio ? $this->id_parque_servicio : '_',
-                  'park' => $this->whenLoaded('park')['nombre_parque'] ? $this->whenLoaded('park')['nombre_parque'] : '_',
-                  'service' => $this->whenLoaded('service')['servicio_nombre'] ? $this->whenLoaded('service')['servicio_nombre'] : '_'
+                  'id' => $this->id_parque_servicio ? $this->id_parque_servicio : '',
+                  'park' => $this->whenLoaded('park')['nombre_parque'] ? $this->whenLoaded('park')['nombre_parque'] : '',
+                  'service' => $this->whenLoaded('service')['servicio_nombre'] ? $this->whenLoaded('service')['servicio_nombre'] : '',
+                  'parkId' => $this->whenLoaded('park')['id_parque'] ? $this->whenLoaded('park')['id_parque'] : '',
+                  'serviceId' => $this->whenLoaded('service')['id_servicio'] ? $this->whenLoaded('service')['id_servicio'] : '',
             ];
       }
 }
