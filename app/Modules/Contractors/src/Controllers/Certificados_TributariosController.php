911c4bdd8c422c1ccfc3a341c89fb5a4c314abdc
MauricioM <simpmed@hotmail.com>
2022-03-07 10:16:54 -0500

    QR y Token
    QR y Token

--- app/Modules/Contractors/src/Controllers/Certificados_TributariosController.php
+++ app/Modules/Contractors/src/Controllers/Certificados_TributariosController.php
@@ -16,6 +16,9 @@ use App\Modules\Contractors\src\Request\ValidacionRequest;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use App\Helpers\FPDF;
+use LaravelQRCode\Facades\QRCode;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class Certificados_TributariosController extends Controller
 {
@@ -32,25 +35,28 @@ class Certificados_TributariosController extends Controller
             ->where('document', $request->get('document'))
             ->where('birthdate', $request->get('birthdate'))
             ->firstOrFail();
-            $certification = new Certification();
-            $certification->document = $contractor->document;
-            $certification->name = $contractor->full_name;
-            $certification->type = "TRB";
-            $certification->save();
+            $certification = Certification::firstOrCreate([
+                "document"=>$request->get('document'),
+                "year"=>$request->get('year'),
+                "type"=>"TRB"
+            ],[
+                "name"=>$contractor->full_name, 
+                "contractor_id"=>$contractor->id   
+            ]);
             $this->dispatch(new VerificationCodeTributario($contractor, $certification));
             $email=mask_email($contractor->email);
             return $this->success_message("Hemos enviado un código de verificación al correo $email.");
-
+            
         } catch (\Exception $exception) {
             if ($exception instanceof ModelNotFoundException) {
                 return $this->error_response(
-                    'No se encuentra el usuario con los parámetros establecidos.',
+                    'Los datos ingresados no son correctos. Verifíquelos e ingréselos nuevamente.',
                     422
                 );
             }
 
             return $this->error_response(
-                'No podemos realizar la consulta en este momento, por favor intente más tarde.',
+                'No podemos realizar la consulta en este momento. Por favor inténtelo más tarde.',
                 422,
                 $exception->getMessage()
             );
@@ -60,28 +66,29 @@ class Certificados_TributariosController extends Controller
 
     public function validarUsuario(ValidacionRequest $request){
         try {
-            Certification::query()
+            $certification=Certification::query()
                 ->where("document", $request->get("document"))
                 ->where("code", $request->get("code"))
+                ->where("type", "TRB")
                 ->firstOrFail();
-            return $this->conexionSeven($request);
+            return $this->conexionSeven($request, $certification);
         } catch (\Exception $exception) {
             if ($exception instanceof ModelNotFoundException) {
                 return $this->error_response(
-                    'El código no coincide con el enviado. Por favor verifique nuevamente',
+                    'El código ingresado no coincide. Verifíquelo nuevamente.',
                     422
                 );
             }
 
             return $this->error_response(
-                'No podemos realizar la consulta en este momento, por favor intente más tarde.',
+                'No podemos realizar la consulta en este momento. Por favor inténtelo más tarde.',
                 422,
                 $exception->getMessage()
             );
         }
     }
 
-    public function conexionSeven(ValidacionRequest $request){
+    public function conexionSeven(ValidacionRequest $request, Certification $certification){
         $http=new Client();
         $response=$http->post("http://66.70.171.168/api/contractors-portal/certificado-tributario/oracle", [
             "json"=>$request->all(), "headers"=>[
@@ -90,7 +97,12 @@ class Certificados_TributariosController extends Controller
             ]
             ]);
             $data=json_decode($response->getBody()->getContents(), true);
-            $pdf=$this->createPDF($data["data"]);
+            if (!isset($data["data"][0])){
+                return $this->error_response(
+                    "NO SE ENCONTRÓ INFORMACIÓN TRIBUTARIA del año {$request->get('year')} para el contratista identificado con el número de documento {$request->get('document')}"
+                );
+            }
+            $pdf=$this->createPDF($data["data"], $certification);
             return $this->success_message([
                "file_name"=>"Ingresos_Retenciones.pdf",
                "file"=>"data:application/pdf;base64,".base64_encode($pdf)
@@ -113,7 +125,7 @@ class Certificados_TributariosController extends Controller
     }
 
 
-    public function createPDF($data){
+    public function createPDF($data, Certification $certification){
         $collection=collect($data??[]);
         $pdf=new FPDF("L", "mm", "Letter");
         $pdf->AddPage();
@@ -121,7 +133,7 @@ class Certificados_TributariosController extends Controller
         $template=$pdf->importPage(1);
         $pdf->useTemplate($template, 0, 0, null, null, true);
         $pdf->SetFont("Helvetica");
-        $pdf->SetFontSize(8);
+        $pdf->SetFontSize(9);
         $pdf->SetTextColor(0,0,1);
         $name=null;
         $document=null;
@@ -141,29 +153,58 @@ class Certificados_TributariosController extends Controller
             $valrete="$ ".number_format($collect["val_rete"]??0, 2, ".", ",");
             $retef[toLower($concepto)]=intval($collect["val_rete"]??0);
             if(toLower($concepto)=="total"){
-                $pdf->Text(90, 146, utf8_decode($valbruto));
+                $pdf->Text(77, 157, utf8_decode($valbruto));
             }else{
-                $pdf->Text(34, 126+($i*4), utf8_decode($collect["liq_nomb"]??""));
-                $pdf->Text(90, 126+($i*4), utf8_decode($valbruto));
-                $pdf->Text(140, 126+($i*4), utf8_decode($valbase));
-                $pdf->Text(175, 126+($i*4), utf8_decode($valrete));
+                $pdf->Text(20, 126+($i*4), utf8_decode($collect["liq_nomb"]??""));
+                $pdf->Text(77, 126+($i*4), utf8_decode($valbruto));
+                $pdf->Text(136, 126+($i*4), utf8_decode($valbase));
+                $pdf->Text(177, 126+($i*4), utf8_decode($valrete));
                 $i++;
             }
         });
         $ret=$retef["rte fuente"]??0;
         $tot=$retef["total"]??0;
-        $pdf->Text(175, 146, utf8_decode(
+        $pdf->Text(177, 157, utf8_decode(
             $ret>1
             ? "$ ".number_format($ret, 2, ".", ",")
             :"$ ".number_format($tot, 2, ".", ",")
             )
         );
-        $pdf->Text(70, 77, utf8_decode($year));
-        $pdf->Text(70, 98, utf8_decode($name));
-        $pdf->Text(70, 106, utf8_decode($document));
-        $pdf->Text(70, 186, utf8_decode(now()->format("Y-m-d H:i:s")));
-
-        return $pdf->Output("S", "Ingresos_Retenciones.pdf", true);
+        $pdf->Text(52, 77, utf8_decode($year));
+        $pdf->Text(52, 100, utf8_decode($name));
+        $pdf->Text(52, 111, utf8_decode($document));
+        $pdf->Text(146, 157, utf8_decode('---'));
+        $pdf->Text(54, 189, utf8_decode(now()->format("Y-m-d H:i:s")));
+        $token = isset($certification->token)?$certification->token:$certification->type.'-'.Str::random(9);
+        $path = env('APP_ENV') == 'local'
+            ? env('APP_PATH_DEV')
+            : env('APP_PATH_PROD');
+        $url = "https://sim.idrd.gov.co/{$path}/es/validar-documento?validate=$token";
+        QrCode::url($url)
+            ->setErrorCorrectionLevel('H')
+            ->setSize(10)
+            ->setOutfile(storage_path("app/templates/{$token}.png"))
+            ->png();
+        $file = storage_path("app/templates/{$token}.png");
+        $pdf->Image($file, 15, 191, 45, 45);
+        $pdf->SetXY(65, 215);
+        $pdf->SetFontSize(7);
+        $pdf->Cell(30, 5, utf8_decode('Lea el QR desde su dispositivo móvil o haga clic en el siguiente enlace para validar la autenticidad del documento:'));
+        $pdf->SetXY(65, 200);
+        $pdf->SetFontSize(8);
+        $pdf->Cell(30, 5, utf8_decode('CÓDIGO DE VERIFICACIÓN: '.$token));
+        $pdf->SetFontSize(7);
+        $pdf->SetXY(65, 220);
+        $pdf->Write(5, $url, $url);
+        if (Storage::disk('local')->exists("templates/{$token}.png")) {
+            Storage::disk('local')->delete("templates/{$token}.png");
+        }
+        if (!isset( $certification->token )) {
+            $certification->token = $token;
+        }
+        $certification->increment('downloads');
+        $certification->save();
+        return $pdf->Output("I", "Ingresos_Retenciones.pdf");
     }
 }
 
